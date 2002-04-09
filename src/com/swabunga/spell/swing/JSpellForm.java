1ed5a8363e83156d24fb8bc5d9c3b9bfb9b941b3
jmheight <jmheight>
2002-04-09 06:27:12 +0000

    Updated to first cut functioning form

--- src/com/swabunga/spell/swing/JSpellForm.java
+++ src/com/swabunga/spell/swing/JSpellForm.java
@@ -1,79 +1,175 @@
-package com.swabunga.spell.swing;
+/*
+ * put your module comment here
+ * formatted with JxBeauty (c) johann.langhofer@nextra.at
+ */
 
-import com.swabunga.spell.event.*;
-import javax.swing.*;
-import javax.swing.text.*;
-import java.io.File;
-import java.awt.*;
-import java.awt.event.*;
+
+package  com.swabunga.spell.swing;
+
+import  com.swabunga.spell.event.*;
+import  javax.swing.*;
+import  javax.swing.text.*;
+import  javax.swing.event.*;
+import  java.io.File;
+import  java.awt.*;
+import  java.awt.event.*;
+
 
 /** Implementation of a spell check form.
- *  <p>
- *  This is no where near complete. It isnt even functioning yet.
+ *  <p>This needs to layed out correctly but for the most part it works.</p>
  *
  * @author Jason Height (jheight@chariot.net.au)
  */
-class JSpellForm extends JPanel {
-  /* GUI COMPONENTS*/
-  private JButton ignoreBtn = new JButton("Ignore");
-  private JButton ignoreAllBtn = new JButton("Ignore All");
-  private JButton addBtn = new JButton("Add");
-  private JButton changeBtn = new JButton("Change");
-  private JButton changeAllBtn = new JButton("Change All");
-  private JButton cancelBtn = new JButton("Cancel");
-  private JList suggestList = new JList();
-  private JTextArea checkText = new JTextArea();
+public class JSpellForm extends JPanel
+    implements ActionListener, ListSelectionListener {
+  /** The Ignore button click action command*/
+  public static final String IGNORE_CMD = "INGORE";
+  /** The Ignore All button click action command*/
+  public static final String IGNOREALL_CMD = "INGOREALL";
+  /** The Add button click action command*/
+  public static final String ADD_CMD = "ADD";
+  /** The Replace button click action command*/
+  public static final String REPLACE_CMD = "REPLACE";
+  /** The Replace All button click action command*/
+  public static final String REPLACEALL_CMD = "REPLACEALL";
+  /** The Cancel button click action command*/
+  public static final String CANCEL_CMD = "CANCEL";
 
-  public JSpellForm() {
+  /* Accessible GUI Components */
+  protected JList suggestList;
+  protected JTextArea checkText;
+  /* The current spell check event */
+  protected SpellCheckEvent spellEvent;
+  /** The listener list (holds actionlisteners) */
+  protected EventListenerList listenerList = new EventListenerList();
+
+  /** Panel constructor */
+  public JSpellForm () {
     initialiseGUI();
   }
 
-  protected JPanel makeEastPanel() {
+  /** Helper method to create a JButton with a command, a text label and a listener*/
+  private static final JButton createButton (String command, String text, ActionListener listener) {
+    JButton btn = new JButton(text);
+    btn.setActionCommand(command);
+    btn.addActionListener(listener);
+    return  btn;
+  }
+
+  /** Creates the buttons on the left hand side of the panel*/
+  protected JPanel makeEastPanel () {
     JPanel jPanel1 = new JPanel();
     jPanel1.setLayout(new BoxLayout(jPanel1, BoxLayout.Y_AXIS));
+    JButton ignoreBtn = createButton(IGNORE_CMD, "Ignore", this);
     jPanel1.add(ignoreBtn);
+    JButton ignoreAllBtn = createButton(IGNOREALL_CMD, "Ignore All", this);
     jPanel1.add(ignoreAllBtn);
+    JButton addBtn = createButton(ADD_CMD, "Add to Dictionary", this);
     jPanel1.add(addBtn);
+    JButton changeBtn = createButton(REPLACE_CMD, "Change", this);
     jPanel1.add(changeBtn);
+    JButton changeAllBtn = createButton(REPLACEALL_CMD, "Change All", this);
     jPanel1.add(changeAllBtn);
+    JButton cancelBtn = createButton(CANCEL_CMD, "Cancel", this);
     jPanel1.add(cancelBtn);
-    return jPanel1;
+    return  jPanel1;
   }
 
-  protected JPanel makeCentrePanel() {
+  protected JPanel makeCentrePanel () {
     JPanel jPanel2 = new JPanel();
     jPanel2.setLayout(new BoxLayout(jPanel2, BoxLayout.Y_AXIS));
-
     JLabel lbl1 = new JLabel("Not in dictionary:");
     jPanel2.add(lbl1);
-
+    checkText = new JTextArea();
     jPanel2.add(new JScrollPane(checkText));
-
     JLabel lbl2 = new JLabel("Suggestions:");
     jPanel2.add(lbl2);
-
+    suggestList = new JList();
+    suggestList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
     jPanel2.add(new JScrollPane(suggestList));
-    return jPanel2;
+    suggestList.addListSelectionListener(this);
+    return  jPanel2;
   }
 
-  protected void initialiseGUI() {
+  /** Called by the constructor to initialise the GUI*/
+  protected void initialiseGUI () {
     setLayout(new BorderLayout());
     this.add(makeEastPanel(), BorderLayout.EAST);
     this.add(makeCentrePanel(), BorderLayout.CENTER);
   }
 
-  /** Shows the form in modal mode and when the user selects a response the
-   *  correct value in the event is set*/
-  public void showModalForm(SpellCheckEvent event) {
-    //JMH TBD
+  /** Register an action listener */
+  public void addActionListener (ActionListener l) {
+    listenerList.add(ActionListener.class, l);
   }
 
-  public static void main(String[] args) {
+  /** Deregister an action listener*/
+  public void removeActionListener (ActionListener l) {
+    listenerList.remove(ActionListener.class, l);
+  }
+
+  protected void fireActionEvent (ActionEvent e) {
+    // Guaranteed to return a non-null array
+    Object[] listeners = listenerList.getListenerList();
+    // Process the listeners last to first, notifying
+    // those that are interested in this event
+    for (int i = listeners.length - 2; i >= 0; i -= 2) {
+      if (listeners[i] == ActionListener.class) {
+        ((ActionListener)listeners[i + 1]).actionPerformed(e);
+      }
+    }
+  }
+
+  /** Sets the current spell check event that is being shown to the user*/
+  public void setSpellEvent (SpellCheckEvent event) {
+    spellEvent = event;
+    DefaultListModel m = new DefaultListModel();
+    java.util.List suggestions = event.getSuggestions();
+    for (int i = 0; i < suggestions.size(); i++) {
+      m.addElement(suggestions.get(i));
+    }
+    suggestList.setModel(m);
+    checkText.setText(event.getInvalidWord());
+  }
+
+  /** Fired when a value in the list is selected*/
+  public void valueChanged(ListSelectionEvent e) {
+    if (!e.getValueIsAdjusting()) {
+      Object selectedValue = suggestList.getSelectedValue();
+      if (selectedValue != null)
+        checkText.setText(selectedValue.toString());
+    }
+  }
+
+  /** Fired when a button is selected */
+  public void actionPerformed (ActionEvent e) {
+    if (IGNORE_CMD.equals(e.getActionCommand())) {
+      spellEvent.ignoreWord(false);
+    }
+    else if (IGNOREALL_CMD.equals(e.getActionCommand())) {
+      spellEvent.ignoreWord(true);
+    }
+    else if (REPLACE_CMD.equals(e.getActionCommand())) {
+      spellEvent.replaceWord(checkText.getText(), false);
+    }
+    else if (REPLACEALL_CMD.equals(e.getActionCommand())) {
+      spellEvent.replaceWord(checkText.getText(), true);
+    }
+    else if (ADD_CMD.equals(e.getActionCommand())) {
+      spellEvent.addToDictionary(checkText.getText());
+    }
+    else if (CANCEL_CMD.equals(e.getActionCommand())) {
+      spellEvent.cancel();
+    }
+    fireActionEvent(e);
+  }
+
+  public static void main (String[] args) {
     try {
       JSpellForm pane = new JSpellForm();
       JFrame frm = new JFrame("Spelling");
       frm.getContentPane().add(pane);
-      frm.setSize(300,300);
+      frm.setSize(300, 300);
       frm.setVisible(true);
       frm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     } catch (Exception ex) {
@@ -81,3 +177,6 @@ class JSpellForm extends JPanel {
     }
   }
 }
+
+
+

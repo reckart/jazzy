5305016c9d5a95360138e7da2666812dcd19a562
wobban <wobban>
2002-04-26 07:17:55 +0000

    Just an interface for Transformators

---
+++ src/com/swabunga/spell/engine/Transformator.java
@@ -0,0 +1,11 @@
+package  com.swabunga.spell.engine;
+
+/** Just am interface for Transformators.
+ *
+ * @author Robert Gustavsson (robert@lindesign.se)
+ */
+public interface Transformator{
+
+    public String transform(String word);
+
+}

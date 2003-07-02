7159ec0056390c007f50a0a51f90e9ee88864429
damieng <damieng>
2003-07-02 12:37:53 +0000

    removed user dictionary (was incompatible with SpellChecker): use SpellDictionaryHashMap instead

--- src/com/swabunga/spell/engine/SpellDictionaryDichoDisk.java
+++ src/com/swabunga/spell/engine/SpellDictionaryDichoDisk.java
@@ -17,23 +17,9 @@ import java.util.*;
  */
 public class SpellDictionaryDichoDisk extends SpellDictionaryASpell {
 
-  /** A field indicating the initial hash map capacity (16KB) for the main
-   *  dictionary hash map. Interested to see what the performance of a
-   *  smaller initial capacity is like.
-   */
-  private final static int INITIAL_CAPACITY = 16 * 1024;
-  /**
-   * The hashmap that contains the personal word dictionary. The map is hashed on the doublemeta
-   * code. The map entry contains a LinkedList of words that have the same double meta code.
-   */
-  protected HashMap persoDictionary = new HashMap(INITIAL_CAPACITY);
-  
   /** Holds the dictionary file for reading*/
   private RandomAccessFile dictFile = null;
   
-  /** Holds the personal dictionary file for appending*/
-  private File persoFile = null;
-
   /** dictionary and phonetic file encoding */
   private String encoding = null;
 
@@ -44,7 +30,6 @@ public class SpellDictionaryDichoDisk extends SpellDictionaryASpell {
     throws FileNotFoundException, IOException {
     super((File) null);
     dictFile = new RandomAccessFile(wordList, "r");
-    readPersoFile();
   }
 
   /**
@@ -55,7 +40,6 @@ public class SpellDictionaryDichoDisk extends SpellDictionaryASpell {
     super((File) null);
     this.encoding = encoding;
     dictFile = new RandomAccessFile(wordList, "r");
-    readPersoFile();
   }
 
   /**
@@ -66,7 +50,6 @@ public class SpellDictionaryDichoDisk extends SpellDictionaryASpell {
     throws FileNotFoundException, IOException {
     super(phonetic);
     dictFile = new RandomAccessFile(wordList, "r");
-    readPersoFile();
   }
   
   /**
@@ -78,81 +61,14 @@ public class SpellDictionaryDichoDisk extends SpellDictionaryASpell {
     super(phonetic, encoding);
     this.encoding = encoding;
     dictFile = new RandomAccessFile(wordList, "r");
-    readPersoFile();
-  }
-  
-  /**
-    * Locate and read the personal dictionary
-    */
-  protected void readPersoFile() {
-    String userHome = System.getProperty("user.home");
-    String osName = System.getProperty("os.name");
-    String filename;
-    if (osName.indexOf("Windows") != -1)
-      filename = "preferences";
-    else
-      filename = userHome + File.separator + ".jaxe_pers_dict";
-    persoFile = new File(filename);
-    try {
-      if (persoFile.exists())
-        createPersoDictionary(new BufferedReader(new FileReader(persoFile)));
-    } catch (Exception ex) {
-      System.err.println(ex.getClass().getName() + ": " + ex.getMessage());
-      persoFile = null;
-    }
   }
   
   /**
    * Add a word permanantly to the dictionary (and the dictionary file).
-   * <p>This needs to be made thread safe (synchronized)</p>
+   * <i>not implemented !</i>
    */
   public void addWord(String word) {
-    putWord(word);
-    if (persoFile == null)
-      return;
-    try {
-      persoFile.createNewFile(); // create new file if and only if necessary
-      FileWriter w = new FileWriter(persoFile.toString(), true); // Open with append.
-      w.write(word);
-      w.write("\n");
-      w.close();
-    } catch (IOException ex) {
-      System.out.println("Error writing to personal dictionary file (" + persoFile.getPath() + ")");
-    }
-  }
-
-  /**
-   * Constructs the personal dictionary from a word list file.
-   * <p>
-   * Each word in the reader should be on a seperate line.
-   * <p>
-   * This is a very slow function, which is why it should be used
-   * only for very small dictionaries
-   */
-  protected void createPersoDictionary(BufferedReader in) throws IOException {
-    String line = "";
-    while (line != null) {
-      line = in.readLine();
-      if (line != null) {
-        line = new String(line.toCharArray());
-        putWord(line);
-      }
-    }
-  }
-
-  /**
-   * Allocates a word in the personal dictionary
-   */
-  protected void putWord(String word) {
-    String code = getCode(word);
-    LinkedList list = (LinkedList) persoDictionary.get(code);
-    if (list != null) {
-      list.add(word);
-    } else {
-      list = new LinkedList();
-      list.add(word);
-      persoDictionary.put(code, list);
-    }
+    System.err.println("error: addWord is not implemented for SpellDictionaryDichoDisk");
   }
 
   /**
@@ -251,9 +167,6 @@ public class SpellDictionaryDichoDisk extends SpellDictionaryASpell {
       System.err.println("IOException: " + ex.getMessage());
       list = new LinkedList();
     }
-    LinkedList persoDictResult = (LinkedList) persoDictionary.get(code);
-    if (persoDictResult != null)
-      list.addAll(persoDictResult);
     return list;
   }
 
